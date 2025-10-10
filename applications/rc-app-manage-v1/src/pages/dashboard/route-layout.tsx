77b0f8bf3acd8ac6abd86601eba5b7122593af29
zizicode <victordoblezz@gmail.com>
2025-10-10 09:55:18 -0400

    fix

--- applications/rc-app-manage-v1/src/pages/dashboard/route-layout.tsx
+++ applications/rc-app-manage-v1/src/pages/dashboard/route-layout.tsx
@@ -1,22 +1,21 @@
-// pages/dashboard/route-layout.tsx
+// route-layout.tsx
 import React from 'react'
 import Layout from '@/components/app/layout'
-import { Routes, Route } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import PostPage from './post/post-page'
 import PostForm from './post/new-post'
 import DashboardPage from './page-dashboar'
 
 const RouterLayout: React.FC = () => {
   return (
-    <Layout>
-      <Routes>
-        {/* Rutas relativas sin / inicial */}
+    <Routes>
+      <Route element={<Layout />}>
         <Route index element={<DashboardPage />} />
         <Route path="posts" element={<PostPage />} />
         <Route path="posts/form" element={<PostForm />} />
         <Route path="posts/form/:id" element={<PostForm />} />
-      </Routes>
-    </Layout>
+      </Route>
+    </Routes>
   )
 }
 
